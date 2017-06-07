34c371ae0b8a1b5ef7af2c5a1d809d9a969ad2fb
AMS\daniel.stashek <daniel.stashek@cgi.com>
2017-06-07 12:05:48 -0600

    added new annotation

--- src/main/java/org/cloudfoundry/Application.java
+++ src/main/java/org/cloudfoundry/Application.java
@@ -3,7 +3,9 @@ package org.cloudfoundry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
